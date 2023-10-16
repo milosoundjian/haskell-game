647c9564f41975de8c2dd886f7d569260a53e5d2
Milos Oundjian <milosoundjian@gmail.com>
2023-10-16 15:58:53 +0200

    Some adjustments to text position and color

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Snake
 
 --  GAME CONSTANTS start here :
 -- screen dimensions are float to reduce number of type casts in computation
-gameWidth :: Float 
+gameWidth :: Float
 gameWidth = 640
 
 gameHeight :: Float
@@ -17,46 +17,37 @@ window :: Display
 window = InWindow "Haskell Puzzle Game" (round gameWidth, round gameHeight) (100, 100)
 
 background :: Color
-background = white
+background = yellow
 
-
---useful shorthand
+-- useful shorthand
 toFloat (x, y) = (fromIntegral x, fromIntegral y)
 
-
---fills up the cell at the input coordinates with the input color
-fillCell :: Position -> Color -> Picture 
+-- fills up the cell at the input coordinates with the input color
+fillCell :: Position -> Color -> Picture
 fillCell (posX, posY) col =
-  let 
-    centerRec = color col (rectangleSolid  cellSize cellSize)
-    topLeftRec = translate (-gameWidth/2  + cellSize/2) 
-                           (-gameHeight/2 + cellSize/2) 
-                           centerRec 
-  in
-    translate ( (fromIntegral posX) * cellSize ) ( (fromIntegral posY) * cellSize) topLeftRec
+  let centerRec = color col (rectangleSolid cellSize cellSize)
+      topLeftRec =
+        translate
+          (-gameWidth / 2 + cellSize / 2)
+          (-gameHeight / 2 + cellSize / 2)
+          centerRec
+   in translate ((fromIntegral posX) * cellSize) ((fromIntegral posY) * cellSize) topLeftRec
 
-
-
---as of now : just display the user character + the current text input 
+-- as of now : just display the user character + the current text input
 render :: GameState -> Picture
 render gameState =
-  let 
-    playerSprite = fillCell (getCharacter gameState) black
+  let playerSprite = fillCell (getCharacter gameState) black
 
-    userText = scale 0.25 0.25 $  
-               translate (-gameWidth * 2) (-gameHeight * 2 + cellSize) (text $ getUserText gameState)
-  in
-    pictures $
-              playerSprite
-              : userText 
-              : []
-
-
-
+      userText =
+        color red $ translate (-gameWidth / 2 + cellSize / 2) (-gameHeight / 2 + cellSize / 2) $
+          scale 0.25 0.25 (text $ getUserText gameState)
+   in pictures $
+        playerSprite
+          : userText
+          : []
 
 update :: Float -> GameState -> GameState
 update seconds gameState = gameState
-
 
 handleKeys :: Event -> GameState -> GameState
 -- handleKeys (EventKey (SpecialKey KeyLeft) Down _ _) gameState = movedGameState gameState LEFT
@@ -64,37 +55,29 @@ handleKeys :: Event -> GameState -> GameState
 -- handleKeys (EventKey (SpecialKey KeyUp) Down _ _) gameState = movedGameState gameState UP
 -- handleKeys (EventKey (SpecialKey KeyDown) Down _ _) gameState = movedGameState gameState DOWN
 
-
---player movement
-handleKeys (EventKey (SpecialKey key) Down _ _ ) gameState
-          | key == KeyLeft = movedGameState gameState LEFT
-          | key == KeyRight = movedGameState gameState RIGHT
-          | key == KeyUp = movedGameState gameState UP
-          | key == KeyDown = movedGameState gameState DOWN
-
+-- player movement
+handleKeys (EventKey (SpecialKey key) Down _ _) gameState
+  | key == KeyLeft = movedGameState gameState LEFT
+  | key == KeyRight = movedGameState gameState RIGHT
+  | key == KeyUp = movedGameState gameState UP
+  | key == KeyDown = movedGameState gameState DOWN
 -- typing into user input
-handleKeys (EventKey (Char ch) Down _ _ ) gs =
-          GameState {
-            getCharacter = getCharacter gs,
-            getUserText = getUserText gs ++ [ch],
-            getRandomStdGen = getRandomStdGen gs
-          }
-
+handleKeys (EventKey (Char ch) Down _ _) gs =
+  GameState
+    { getCharacter = getCharacter gs,
+      getUserText = getUserText gs ++ [ch],
+      getRandomStdGen = getRandomStdGen gs
+    }
 -- removing characters from user input
 handleKeys (EventKey (SpecialKey KeyDelete) Down _ _) gs =
-          if (length (getUserText gs) > 0) then 
-            GameState {
-              getCharacter = getCharacter gs,
-              getUserText = init (getUserText gs),
-              getRandomStdGen = getRandomStdGen gs
-            }
-          else
-            gs 
-
-
-
-
-
+  if (length (getUserText gs) > 0)
+    then
+      GameState
+        { getCharacter = getCharacter gs,
+          getUserText = init (getUserText gs),
+          getRandomStdGen = getRandomStdGen gs
+        }
+    else gs
 handleKeys _ gameState = gameState
 
 main :: IO ()
--- src/Snake.hs
+++ src/Snake.hs
@@ -5,14 +5,13 @@ import System.Random
 
 data Direction = UP | DOWN | LEFT | RIGHT deriving (Eq, Ord)
 
-
 -- tuple of form (x, y)
 -- reminder : coordinates start at BOTTOM LEFT of the screen
 -- (0, 0) is the bottom left corner of the viewport
 -- coordinates are in the range (0, 0) -> (cols - 1, rows - 1)
 type Position = (Int, Int)
 
-type Character = Position 
+type Character = Position
 
 cols :: Int
 cols = 16
@@ -20,7 +19,7 @@ cols = 16
 rows :: Int
 rows = 12
 
-cellSize :: Float 
+cellSize :: Float
 cellSize = 40
 
 directionVectorMap :: Map Direction (Int, Int)
@@ -30,44 +29,36 @@ directionVectorMap =
       [UP, DOWN, LEFT, RIGHT]
       [(0, 1), (0, (-1)), ((-1), 0), (1, 0)]
 
-clamp :: (Ord a) => a -> a -> a -> a 
-clamp mn mx = max mn . min mx 
+clamp :: (Ord a) => a -> a -> a -> a
+clamp mn mx = max mn . min mx
 
 -- executes the move in the given direction
 -- if the move is illegal : do nothing
-move :: Character -> Direction -> Character 
+move :: Character -> Direction -> Character
 move (charX, charY) direction =
-  let 
-    dirVector = directionVectorMap ! direction
-    newChar = (charX + fst dirVector, charY + snd dirVector)
-  in
-    (clamp 0 (cols-1) (fst newChar), clamp 0 (rows-1) (snd newChar))
-
+  let dirVector = directionVectorMap ! direction
+      newChar = (charX + fst dirVector, charY + snd dirVector)
+   in (clamp 0 (cols - 1) (fst newChar), clamp 0 (rows - 1) (snd newChar))
 
 -- same as move but takes and returns game state instead of char
 movedGameState :: GameState -> Direction -> GameState
-movedGameState gs dir = 
-  GameState {
-    getCharacter = move (getCharacter gs) dir,
-    getUserText = getUserText gs,
-    getRandomStdGen = (getRandomStdGen gs)
-  }
-
+movedGameState gs dir =
+  GameState
+    { getCharacter = move (getCharacter gs) dir,
+      getUserText = getUserText gs,
+      getRandomStdGen = (getRandomStdGen gs)
+    }
 
 data GameState = GameState
-  { 
-    getCharacter :: Character,
+  { getCharacter :: Character,
     getUserText :: String,
     getRandomStdGen :: StdGen
   }
 
-
-
 initialGameState :: GameState
-initialGameState  =
+initialGameState =
   GameState
-    { 
-      getCharacter = (10, 10),
+    { getCharacter = (10, 10),
       getUserText = "[Commands appear here]",
       getRandomStdGen = mkStdGen 100
     }
