5c4cef1a2a8cd2a99db32f625d258b79fb1b7811
Rali Lahlou <rali.lahlou@jabiru.polytechnique.fr>
2023-10-18 14:54:52 +0200

    Worst code 2023

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -1,12 +1,15 @@
 module Interpreter where
 
+import Prelude hiding (lex)
 import Data.Char
 import Graphics.Gloss
 
 import DataTypes
 
 -- This is the file that will interpret user commands
-data Token = OP String | Pos Position | Name String
+data Token = OP String | Pos Position | Name String 
+             deriving Show
+                
 type Command = [Token]
 
 ignoredChars :: [Char]
@@ -25,28 +28,44 @@ rmESpaces input | (last input == ' ') = init input
 preprocess :: String -> String
 preprocess input = toLower <$> (rmFSpaces . rmESpaces $ input)
 
--- transforms our input string into a list of tokens
+-- student makes WORST parser EVER, asked to LEAVE CSE301
 lex :: String -> Command
-lex input = undefined
-    -- 1h30 spent on this : i can't do this anymore  
+lex input = 
+    case input of 
+        "" -> []
+        (c:rest) | elem c ignoredChars -> lex rest
 
-    -- case input of 
-    --     "" -> []
-    --     (c:rest) | (elem c ignoredChars) -> parse rest
-    --     ('a':'d':'d':rest) -> (OP "add") : parse rest
-    --     ('s':'u':'d':rest) -
+        --parsing operations 
+        ('a':'d':'d':rest) -> OP "add" : lex rest
+        ('s':'u':'b':rest) -> OP "sub" : lex rest
+
+        --parsing tuples (terrible code) (TODO : should use multi-pass approach)
+        ('(':x:',':y:')':rest) | isDigit x && isDigit y -> 
+            Pos (digitToInt x, digitToInt y) : lex rest
+        ('(':x1:x2:',':y:')':rest) | and (isDigit <$> [x1, x2, y]) -> 
+            Pos (digitToInt x1 * 10 + digitToInt x2, digitToInt y) : lex rest
+        ('(':x:',':y1:y2:')':rest) | and (isDigit <$> [x, y1, y2]) -> 
+            Pos (digitToInt x, digitToInt y1 * 10 + digitToInt y2) : lex rest
+        ('(':x1:x2:',':y1:y2:')':rest) | and (isDigit <$> [x1, x2, y1, y2]) -> 
+            Pos (digitToInt x1 * 10 + digitToInt x2, digitToInt y1 * 10 + digitToInt y2) : lex rest
+        
+        --all lonely characters will be merged eventually
+        (c:rest) -> Name (c:[]) : lex rest
+
+mergeNames :: Command -> Command 
+mergeNames [] = []
+mergeNames (Name a:Name b:rest) = mergeNames (Name (a ++ b):rest)
+mergeNames (x:xs) = x:(mergeNames xs)
 
 
-
-      
 
 
 
 interpret :: String -> GameState -> GameState
 interpret input gameState =
     let
-        command = preprocess input
+        command = mergeNames . lex . preprocess $ input
     in
-        gameState
+        gameState {userText = show command }
 
 
