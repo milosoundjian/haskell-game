0f1781e310780d99633d95020919ae3b0877b568
Milos Oundjian <milosoundjian@gmail.com>
2023-10-18 14:56:01 +0200

    Add new assets

--- src/Level.hs
+++ src/Level.hs
@@ -1,20 +1,24 @@
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
 module Level where
 
+import Constants
 import Data.Tree
 import DataTypes
 
-type Obstacles = [Position]
+type Boxes = [Position]
 
 type Spikes = [Position]
 
+type Water = [Position]
+
 data Room = Room
-    {
-        spikes :: Spikes,
-        obstacles :: Obstacles,
-        isTerminal :: Bool,
-        getSpecialPos :: Position, 
-        initialCharacterPos :: Position
-    }
+  { spikes :: Spikes,
+    boxes :: Boxes,
+    water :: Water,
+    isTerminal :: Bool,
+    getSpecialPos :: Position,
+    initialCharacterPos :: Position
+  }
 
 type Level = Tree Room
