ee8a50b546c6b3efd5499080f88074429ef6c822
Apllify <ralilahlou50@gmail.com>
2023-10-31 20:07:54 +0100

    Level 0-1 complete for now

--- src/LevelData.hs
+++ src/LevelData.hs
@@ -46,6 +46,24 @@ room0A :: RoomState
 room0A = 
     roomBase 
     {
+        character= (1, 1),
+        charRot = 180,
+
+        waters = [(0, 7), (0, 6), (0, 5), (0, 4), (1, 7), (1, 6), (1, 5),
+                    (2, 7), (2, 6), (2, 5), (3, 7), (3, 6), (3, 0), (4, 7),
+                    (4, 6), (4, 1), (4, 0), (5, 7), (5, 6), (5, 2), (5, 1),
+                    (5, 0), (6, 7), (6, 3), (6, 2), (6, 1), (6, 0), (7, 3),
+                    (7, 2), (7, 1), (7, 0), (8, 4), (8, 3), (8, 2), (8, 1),
+                    (8, 0), (9, 5), (9, 4), (9, 3), (9, 2), (9, 1), (9, 0)],
+
+        isTerminal = True,
+        specialPos = (8, 6)
+    }
+
+room1A :: RoomState
+room1A = 
+    roomBase 
+    {
         character = (9, 1),
         charRot = 180,
 
@@ -65,12 +83,34 @@ room0A =
 
 
 
--- room1A :: RoomState 
--- room1A = 
---     roomBase 
---     {
+room2A, room2B, room2C :: RoomState 
+room2A = 
+    roomBase 
+    {
+        character = (0, 4),
+        charRot = -90,
 
---     }
+        waters = [(4, 7), (4, 6), (4, 3), (4, 2), (4, 1), (4, 0), (5, 7),
+                    (5, 6), (5, 3), (5, 2), (5, 1), (5, 0)],
+        spikes = [(0, 7), (0, 0), (1, 6), (1, 1), (2, 7), (2, 2), (3, 6),
+                      (3, 3), (6, 6), (6, 3), (7, 7), (7, 2), (8, 6), (8, 1),
+                      (9, 7), (9, 0)],
+
+        isTerminal = False,
+        specialPos = (9, 4)
+    }
+
+room2B = roomDebug
+    -- roomBase 
+    -- {
+
+    -- }
+
+room2C = roomDebug
+    -- roomBase 
+    -- {
+
+    -- }
 
 --eidolon game state 
 gsBase :: GameState
@@ -120,18 +160,25 @@ level1 :: Level
 level1 = [
 
     startScreen{
-        screen = [room0A]
-    },
-
-    midScreen{
-        screen = [room0A]
+        screen = [room1A]
     }
 
     ]
 
+level2 :: Level 
+level2 = 
+    [
+        startScreen{
+            screen = [room2A]
+        },
+        midScreen {
+            screen = [room2B, room2C]
+        }
+    ]
+
 -- Combining everything
 levelsData:: LevelData
-levelsData = level0 ++ level1
+levelsData = level0 ++ level1 ++ level2
 
 titles :: [Title]
-titles = ["0 : Getting Started", "1 : Binary Bifurcation Branch"]
+titles = ["0 : Initiation", "1 : The Pursuit of Happiness", "2 : Roundabout"]
