b664411f32e96078abd4591d03b708ed87280f19
Brook-B-Nigatu <burabalke7776@gmail.com>
2023-11-04 17:25:17 +0100

    Lines in the pause screen for edges

--- src/PauseScreen.hs
+++ src/PauseScreen.hs
@@ -7,21 +7,38 @@ import Constants
 import Graphics.Gloss
 
 
+-- Compute the final picture to be drawn on the pause screen from the sprites and the screen pointer in GameState
+-- First Sprite is for the current Position in the tree.
+-- Second Sprite is for unlocked puzzles.
+-- Third Sprite is for locked puzzles.
 getTreePic :: Sprite -> Sprite -> Sprite -> BinZip ScreenWrap -> Picture
 getTreePic activePic visitedPic toVisitPic sp =
   let 
+    -- convert the screen tree to a sprite tree
     spriteTree = getScreenSprite activePic visitedPic toVisitPic <$> toTree sp
   in
-    fromSpriteTree (0, gameHeight / 2 - 80, gameWidth / 4, 50) spriteTree
+    pictures $ fromSpriteTree (0, gameHeight / 2 - gameHeight / 10, gameWidth / 4, 50) spriteTree
 
-fromSpriteTree :: (Float, Float, Float, Float) -> BinTree Sprite -> Picture
-fromSpriteTree _ Leaf = pictures []
-fromSpriteTree (x, y, dx, dy) (B s@(Sprite{dimensions = (w, h), picture = pic}) lsub rsub) =
+-- compute the list of pictures to be rendered from the sprite tree constructed above
+fromSpriteTree :: (Float, Float, Float, Float) -> BinTree Sprite -> [Picture]
+fromSpriteTree _ Leaf = []
+fromSpriteTree (x, y, dx, dy) (B s@(Sprite{picture = pic}) lsub rsub) =
   let 
-    lpic = fromSpriteTree (x - dx, y - dy, dx / 2, dy) lsub
-    rpic = fromSpriteTree (x + dx, y - dy, dx / 2, dy) rsub
+    -- recursive computation
+    lpics = fromSpriteTree (x - dx, y - dy, dx / 2, dy) lsub
+    rpics = fromSpriteTree (x + dx, y - dy, dx / 2, dy) rsub
+    
+    -- red colored lines for edges
+    col = makeColor 1.0 0 0 1.0 
+    linel = color col $ Line [(x, y), (x - dx, y - dy)]
+    liner = color col $ Line [(x, y), (x + dx, y - dy)]
+
+    -- only draw lines for non-leaf children
+    lpics' = if null lpics then lpics else linel : lpics
+    rpics' = if null rpics then rpics else liner : rpics
+
   in
-    pictures [translate x y pic, lpic, rpic]
+    lpics' ++ rpics' ++ [translate x y pic]
 
 
 
